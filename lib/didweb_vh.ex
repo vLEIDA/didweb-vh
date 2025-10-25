b062d5d182c214d5f9af5c7904e2cf2feb818cb3
Charles Lanahan <charles.lanahan@gmail.com>
2025-10-25 10:51:24 -0400

    New Mix project commit

---
+++ lib/didweb_vh.ex
@@ -0,0 +1,18 @@
+defmodule DidwebVh do
+  @moduledoc """
+  Documentation for `DidwebVh`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DidwebVh.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/didweb_vh_test.exs
@@ -0,0 +1,8 @@
+defmodule DidwebVhTest do
+  use ExUnit.Case
+  doctest DidwebVh
+
+  test "greets the world" do
+    assert DidwebVh.hello() == :world
+  end
+end
